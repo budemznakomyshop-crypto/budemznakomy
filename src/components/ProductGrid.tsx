0d3dd8b7fff83570c27f0455deaebbff154d342e
PribislavHippe <kordvaduo2@gmail.com>
2025-11-19 11:45:00 +0300

    images 1

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -10,7 +10,7 @@ interface ProductGridProps {
 
 const beansProducts = [
   {
-    image: "https://images.unsplash.com/photo-1663911278520-0fa05ff61eaf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb2ZmZWUlMjBiZWFucyUyMGNsb3NlJTIwdXB8ZW58MXx8fHwxNzYyODMyNzUwfDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
+    image: "ethiopia.jpg",
     name: "Эфиопия Арричи",
     processing: "Мытая обработка",
     weight: "250 г",
@@ -18,7 +18,7 @@ const beansProducts = [
     price: 680,
   },
   {
-    image: "https://images.unsplash.com/photo-1663911278520-0fa05ff61eaf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb2ZmZWUlMjBiZWFucyUyMGNsb3NlJTIwdXB8ZW58MXx8fHwxNzYyODMyNzUwfDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
+    image: "kostarica.jpg",
     name: "Коста-Рика Тарразу",
     processing: "Мытая обработка",
     weight: "250 г",
